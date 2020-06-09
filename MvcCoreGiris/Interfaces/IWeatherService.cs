7fd15ae5aa0e2ca31995211d266c1dacd6e091ab
boranb <berkayboran@gmail.com>
2020-06-09 21:59:33 +0300

    dependency injection & view components

---
+++ MvcCoreGiris/Interfaces/IWeatherService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreGiris.Interfaces
+{
+    public interface IWeatherService
+    {
+        public decimal Temperature(string cityName);
+    }
+}
--- MvcCoreGiris/Startup.cs
+++ MvcCoreGiris/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcCoreGiris.Interfaces;
 using MvcCoreGiris.Models;
 using MvcCoreGiris.Services;
 
@@ -30,6 +31,7 @@ namespace MvcCoreGiris
             // https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-3.1&tabs=visual-studio
             services.AddDbContext<OkulContext>(options => options.UseSqlServer(Configuration.GetConnectionString("OkulContext")));
             services.AddSingleton<LuckyNumberService>();
+            services.AddScoped<IWeatherService, OpenWeatherMapService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
